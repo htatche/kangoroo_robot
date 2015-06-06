f4798dc1d7671142f2f259697d497b2410c6d03b
Herve Tatche <herve.tatche@gmail.com>
2015-06-06 22:59:59 +0200

    New Instruction::Right and Instruction::Right classes
    Added classes that enable rotation of 90 degrees both to left and right
    Added suite of tests for both

---
+++ lib/instructions/left.rb
@@ -0,0 +1,31 @@
+require "instruction"
+
+class Instruction::Left < Instruction 
+
+  def initialize
+  end
+
+  def rotate(direction)
+    case direction.cardinal_point
+      when "NORTH"
+        "WEST"
+      when "WEST"
+        "SOUTH"
+      when "SOUTH"
+        "EAST"
+      when "EAST"
+        "NORTH"
+    end
+  end
+
+  def execute(position, direction)
+
+    if !position.settled
+      # Reporter::say(:is_not_placed)
+      return false
+    end
+
+    direction.cardinal_point = rotate(direction)
+  end
+
+end
---
+++ lib/instructions/right.rb
@@ -0,0 +1,31 @@
+require "instruction"
+
+class Instruction::Right < Instruction 
+
+  def initialize
+  end
+
+  def rotate(direction)
+    case direction.cardinal_point
+      when "NORTH"
+        "EAST"
+      when "EAST"
+        "SOUTH"
+      when "SOUTH"
+        "WEST"
+      when "WEST"
+        "NORTH"
+    end
+  end
+
+  def execute(position, direction)
+
+    if !position.settled
+      # Reporter::say(:is_not_placed)
+      return false
+    end
+
+    direction.cardinal_point = rotate(direction)
+  end
+
+end
